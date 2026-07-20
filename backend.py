0708dae541110c64d8b2d7bd8d89d7f384b75d79
Saurabh <saurabh11308@yahoo.com>
2026-07-20 18:56:43 +0530

    Added the images and the code delta

--- backend.py
+++ backend.py
@@ -1,8 +1,15 @@
 import requests
+API_KEY = "f914a30a25e0980f477c6786a25d19f8"
 
-def get_data():
-    pass
+
+def get_data(city,days):
+    response = requests.get(f"http://api.openweathermap.org/data/2.5/forecast"
+                            f"?q={city}&appid={API_KEY}")
+    response_j = response.json()
+    days_data = response_j['list'][:days*8]
+    temp_list = [day_data['main']['temp']/10 for day_data in days_data]
+    print(response_j)
 
 
 if __name__ == "__main__":
-    get_data()
+    get_data("Airoli",2)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from cProfile import label
+
 import streamlit as st
 from  backend import get_data
-
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 city = st.text_input(label="Place",placeholder="Enter city name :-")
@@ -8,3 +10,11 @@ days = st.slider(label="Forecast days",min_value=1,max_value=5,
                          help="Please select a value between 1 and 5")
 type = st.selectbox("Select data to view",("Temperature","Sky"))
 st.subheader(f"{type} for the next {days} days in {city}")
+
+date = ["2026-20-07","2026-19-07","2026-18-07"]
+temprature = [6,7,15]
+temprature = [days*i for i in temprature]
+
+figure = px.line(x=date,y=temprature,labels={"x":"Dates","y":"Tempratures ("\
+                                                             "C)"})
+st.plotly_chart(figure)
