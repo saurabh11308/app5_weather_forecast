635abdad5cb788755f279a1814933a7b74a5ed99
Saurabh <saurabh11308@yahoo.com>
2026-07-20 17:59:38 +0530

    Added the images and the code delta

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import streamlit as st
 from  backend import get_data
 
+
+st.title("Weather Forecast for the Next Days")
+city = st.text_input(label="Place",placeholder="Enter city name :-")
+days = st.slider(label="Forecast days",min_value=1,max_value=5,
+                         help="Please select a value between 1 and 5")
+type = st.selectbox("Select data to view",("Temperature","Sky"))
+st.subheader(f"{type} for the next {days} days in {city}")
